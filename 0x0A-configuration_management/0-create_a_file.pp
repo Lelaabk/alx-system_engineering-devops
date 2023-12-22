a9ab35a2f95bdfc1b6f12f372c5acb79064501bf
Lelaabk <lolabkari@gmail.com>
2023-12-22 14:44:57 -0800

    Create File

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# 0-create_a_file.pp
+# Create the file in /tmp
+
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
