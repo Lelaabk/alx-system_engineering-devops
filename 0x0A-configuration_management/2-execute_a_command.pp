66e07d782a05d33bbe40033800676ff328c296af
Lelaabk <lolabkari@gmail.com>
2023-12-22 16:17:53 -0800

    Execute command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # 2-execute_a_command.pp
 # Define exec resource to kill the process
 exec { 'killmenow':
-  command     => 'pkill -f killmenow',
-  path        => '/bin/',
+  command => 'pkill killmenow',
+  path    => '/bin/',
 }
