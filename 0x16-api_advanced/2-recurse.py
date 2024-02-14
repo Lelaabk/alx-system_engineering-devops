fcb4b511b58c42fcd6daf2f3fd5b31d96579d2de
Lelaabk <lolabkari@gmail.com>
2024-02-14 03:18:42 -0800

    Recurse

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,13 +3,15 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], count=0, after=''):
+def recurse(subreddit, hot_list=[], count=0, after='i'):
     """
     Queries Reddit Api and returns list containing titles of all hot articles.
     """
     site = "https://www.reddit.com"
     endpoint = 'r/{}/hot.json'.format(subreddit)
-    query = '?show="all"&limit=100&after={}&count={}'.format(after, count)
+    query = '?show="all"&limit=100&count={}'.format(count)
+    if after:
+        query += '&after={}'.format(after)
     url = site + endpoint + query
     headers = {'User-Agent': 'My Reddit API Client by /u/lelaabk'}
     response = requests.get(url, headers=headers,
@@ -24,9 +26,9 @@ def recurse(subreddit, hot_list=[], count=0, after=''):
     data = response.json()['data']
     for post in data['children']:
         hot_list.append(post['data']['title'])
-    after = data['after']
+    after = data.get('after')
     dist = data['dist']
-    if (after):
+    if after:
         recurse(subreddit, hot_list, count + dist, after)
 
     if len(hot_list) == 0:
