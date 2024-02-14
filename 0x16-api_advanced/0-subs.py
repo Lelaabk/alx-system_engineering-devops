15783628f93043f1fa5d07bb3a087d4eefb25dee
Lelaabk <lolabkari@gmail.com>
2024-02-14 02:30:14 -0800

    Subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,13 +7,12 @@ def number_of_subscribers(subreddit):
     """
     Queries Reddit Api and returns number of subscribers for given subreddit.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent' : 'Taha'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 0)'}
+    response = requests.get(url, headers=headers,
+                            allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    if (not response.ok):
         return 0
+    subscribers = response.json().get('data').get('subscribers')
+    return subscribers
