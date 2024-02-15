529203d7f0236bc18138bb56696943da7abd64ad
Lelaabk <lolabkari@gmail.com>
2024-02-14 16:55:37 -0800

    Tmux

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,7 @@
 # Puppet manifest to fix Appache 500 error by strace
-service { 'apache2':
-  ensure => running,
+# Fix bad extension to "php" in this file 'wp-settings.php'
+
+exec{'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => 'usr/local/bin/:/bin/'
 }
